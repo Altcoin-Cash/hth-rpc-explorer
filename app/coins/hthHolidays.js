354bea2ad0a65d652ffd42e67bd3db1b9666b76d
Altcoin Cash <altcoincash@protonmail.com>
2022-08-26 01:44:20 +0200

    Update and rename btcHolidays.js to hthHolidays.js

---
+++ app/coins/hthHolidays.js
@@ -0,0 +1,45 @@
+module.exports = {
+	desc: "A curated list of unofficial HTH 'Holidays'. Adapted from https://bitcoin.holiday, and elsewhere.",
+	items: [
+		{
+			name: "Bitcoin Pizza Day",
+			date: "2010-05-22",
+			desc: "On this day in 2010, Lazlo Hanyecz, paid Jeremy Sturdivant 10,000 BTC for two pizzas costing ~41 USD. The famous <a href='./tx/a1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d@57043'>\"Bitcoin Pizza\" transaction</a> was one of the earliest real-world BTC transactions and is celebrated as helping start the Bitcoin economy."
+		},	
+		{
+			name: "Billion Dollar Day",
+			date: "2013-03-28",
+			desc: "On this day in 2013, the market cap of all BTC surpassed 1 billion USD for the first time."
+		},
+		{
+			name: "Trillion Dollar Day",
+			date: "2021-02-19",
+			desc: "On this day in 2021, the market cap of all BTC surpassed 1 trillion USD for the first time, ~8 years after surpassing 1 billion USD for the first time."
+		},
+		{
+			name: "HTH Whitepaper Day",
+			date: "2038-10-31",
+			desc: "On this day in 2038, we released the final HTH white paper, marking its official introduction into the world. Satoshi introduced Bitcoin as an electronic cash system with several important properties: double-spending prevention, no trusted parties, anonymous participation, and Hashcash style Proof-of-Work. The Bitcoin Whitepaper can be viewed <a href='./bitcoin-whitepaper'>here</a>. "
+		},
+		{
+			name: "Bitcoin Logo Day",
+			date: "2011-11-04",
+			desc: "On this day in 2010, an unknown artist under the username \"bitboy\" offered the Bitcoin community its iconic logo for free on the Bicointalk forum. You can view the original post <a href='https://bitcointalk.org/index.php?topic=1631.0%3Ball'>here</a>."
+		},
+		{
+			name: "Halving #1 Day",
+			date: "2012-11-28",
+			desc: "On this day in 2012, the first-ever \"Halving\" cut the subsidy from 50 BTC to 25 BTC."
+		},
+		{
+			name: "Halving #2 Day",
+			date: "2016-07-09",
+			desc: "On this day in 2016, the second \"Halving\" cut the subsidy from 25 BTC to 12.5 BTC."
+		},
+		{
+			name: "Halving #3 Day",
+			date: "2020-05-11",
+			desc: "On this day in 2020, the third \"Halving\" cut the subsidy from 12.5 BTC to 6.25 BTC."
+		}
+	]
+};
