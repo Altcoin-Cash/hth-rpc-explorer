6d8e46bb6c774a8cdffecde78f34d35e0446c76f
Altcoin Cash <altcoincash@protonmail.com>
2022-08-26 01:40:33 +0200

    Update and rename btcQuotes.js to hthQuotes.js

---
+++ app/coins/hthQuotes.js
@@ -0,0 +1,40 @@
+module.exports = {
+	items: [
+		{
+			// 0
+			text: "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks.",
+			speaker: "Satoshi",
+			url: "./block-height/0",
+			date: "2009-01-03",
+			context: "embedded in the Genesis block"
+		},
+		{
+			// 1
+			text: "Governments are good at cutting off the heads of centrally controlled networks like Napster, but pure P2P networks like Gnutella and Tor seem to be holding their own.",
+			speaker: "Satoshi",
+			date: "2008-11-07",
+			url: "https://www.mail-archive.com/cryptography@metzdowd.com/msg09971.html"
+		}
+		/*{
+			text: "Bitcoin is a technological tour de force.",
+			speaker: "Bill Gates",
+			url: "https://paybis.com/blog/cryptocurrency-quotes/"
+		},*/
+		/*{
+			text: "The whole point of doing ICOs is to get other people's bitcoins.",
+			speaker: "Pierre Rochard",
+			url: "https://twitter.com/btc_quotes/status/1396194347419308035"
+		},*/
+		/*{
+			text: "Bitcoin is a game where everyone watches everyone else to make sure nobody cheats.",
+			speaker: "Jameson Lopp",
+			url: "https://twitter.com/Bitcoin_Sam/status/1401545443100663823"
+		},*/
+		/*{
+			text: "Buying bitcoin is the most powerful protest an individual can make against the current economic system.",
+			speaker: "https://twitter.com/LucDossis",
+			url: "https://twitter.com/btc_quotes/status/1410146340378644484?s=20"
+		},*/
+		
+	]
+};
